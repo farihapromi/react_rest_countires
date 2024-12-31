451401f8cb563293c49b2f28d9e91cab1549979d
Fariha Promi <farihapromi2611@gmail.com>
2024-12-31 21:53:15 +0600

    add rest_countires

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import Countries from './componnets/Country/Countries';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Countries></Countries>
+      
+      <LoadCountries></LoadCountries>
+      
     </div>
   );
 }
+/* function LoadCountries(){
+  const[countries,setCountries]=useState([])
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=>res.json())
+    .then(data=>setCountries(data))
+  },[])
+  return(
+    <div>
+      <h1>Visiitng Every country of the world</h1>
+      {
+        countries.map(country=><Country name={country.name.common}></Country>)
+      }
+    </div>
+  )
+} */
+
+/* function Country(props){
+  return(
+    <div>
+      <h2>Name:{props.name}</h2>
+    </div>
+  )
+} */
 
 export default App;
---
+++ src/componnets/Country/Countries.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Countries() {
+  return (
+    <div>Country
+        <h2>Countries in other </h2>
+    
+    
+    </div>
+  )
+}
+
+export default Countries
